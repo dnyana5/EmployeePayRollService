319252a91b49b7349e4f17886589cf9896fcda5a
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-01-23 09:18:09 +0530

    Added:Welcome message

---
+++ src/main/java/com/employeepayroll/EmployeePayrollService.java
@@ -0,0 +1,15 @@
+package com.employeepayroll;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Pay Roll Service Code");
+    }
+
+}
+
