e691c136e5fcca3e18756d1fc70ad3f0798eaff4
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-01-23 09:27:59 +0530

    Added:created Employee pay roll service class to add employee details to the console

--- src/main/java/com/employeepayroll/EmployeePayrollService.java
+++ src/main/java/com/employeepayroll/EmployeePayrollService.java
@@ -5,11 +5,33 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
-
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Pay Roll Service Code");
+    private List<EmployeePayrollData> employeePayrollList;
+    public EmployeePayrollService() {
+    }
+    public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList){
+        this.employeePayrollList = employeePayrollList;
     }
 
+    public static void main(String[] args) {
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData();
+    }
+
+    private void readEmployeePayrollData(Scanner consoleInputReader) {
+        System.out.println("Enter Employee id");
+        int id = consoleInputReader.nextInt();
+        System.out.println("Enter Employee name");
+        String name = consoleInputReader.next();
+        System.out.println("Enter Employee salary");
+        double salary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+    }
+
+    private void writeEmployeePayrollData() {
+        System.out.println("\nWriting Employee Payroll Roaster to Console\n" +employeePayrollList);
+    }
 }
 
